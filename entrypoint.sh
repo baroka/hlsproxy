7bb4ddd0153c7176f728d6c1f02c73500b5665d5
baroka <14985106+baroka@users.noreply.github.com>
2022-04-10 10:42:28 +0200

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,11 +21,11 @@ echo "------ Log file is $LOG_FILE -------"
 echo "       Do not modify it"
 > $LOG_FILE
 jq '.LOG.file.isEnabled=true' $CONFIG_FILE > $TEMP_FILE
-cp $TEMP_FILE $CONFIG_FILE
+cat $TEMP_FILE > $CONFIG_FILE
 jq '.LOG.file.filename="/var/log/hls-proxy.log"' $CONFIG_FILE > $TEMP_FILE
-cp $TEMP_FILE $CONFIG_FILE
+cat $TEMP_FILE > $CONFIG_FILE
 jq '.LOG.file.backups=1' $CONFIG_FILE > $TEMP_FILE
-cp $TEMP_FILE $CONFIG_FILE
+cat $TEMP_FILE > $CONFIG_FILE
 rm $TEMP_FILE
 
 echo '------ HLS PROXY -------'
